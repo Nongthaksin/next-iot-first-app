094ee374384aa4cbd6c4da2d96a3b6098015733a
Nongthaksin <fordthaksin@gmail.com>
2026-01-20 10:26:23 +0700

    what the fuck

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
       <h1 className="text-center font-bold mt-10">Home Page</h1>
-      <h3 className="text-center text-blue-600 font-bold">ID:6519410019</h3>
-      <h3 className="text-center">Natthawut Sukto</h3>
+      <h3 className="text-center text-blue-600 font-bold">ID:6519410052</h3>
+      <h3 className="text-center">Thaksin Aueakit</h3>
       <hr />
       <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et fugiat veritatis quas aspernatur eligendi reiciendis eum iusto quasi nostrum nisi?</p>
     </>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
     <hr className='my-10 w-3/4 m-auto' />
     <h5 className='text-center'>
-        Create By Natthawut ©️ 2026
+        Create By Thaksin * 2026
     </h5>
     </>
   )
